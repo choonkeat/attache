370d6e50cbdd016d507925eecca5f70c4f2e0ad3
choonkeat <choonkeat@gmail.com>
2015-04-05 12:45:25 +0800

    Missed adding file for commit 752d421

---
+++ spec/lib/attache/vhost_spec.rb
@@ -0,0 +1,23 @@
+require 'spec_helper'
+
+describe Attache::VHost do
+  let(:config) { { 'REMOTE_DIR' => remotedir } }
+  let(:vhost) { Attache::VHost.new(config) }
+  let(:remote_api) { double(:remote_api) }
+  let(:file_io) { StringIO.new("") }
+  let(:relpath) { 'relpath' }
+  let(:cachekey) { 'hostname/relpath' }
+  let(:remotedir) { 'remote_directory' }
+
+  before do
+    allow(vhost).to receive(:remote_api).and_return(remote_api)
+  end
+
+  describe '#storage_create' do
+    it 'should read with cachekey, write with remotedir prefix' do
+      expect(Attache.cache).to receive(:read).with(cachekey).and_return(file_io)
+      expect(remote_api).to receive(:create).with(key: "#{remotedir}/#{relpath}", body: file_io)
+      vhost.storage_create(relpath: relpath, cachekey: cachekey)
+    end
+  end
+end
